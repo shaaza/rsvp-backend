32c7894f4129b6b116f96ac63498a6b0ea1709a8
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-09-10 20:17:51 +0530

    Add cheshire as dependency.

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[bidi "1.19.0"]
+                 [cheshire "5.4.0"]
                  [clj-http "2.0.0"]
-                 [com.taoensso/timbre "2.7.1"]
                  [com.taoensso/faraday "1.9.0"]
+                 [com.taoensso/timbre "2.7.1"]
                  [levand/immuconf "0.1.0"]
                  [medley "0.7.0"]
                  [org.clojure/clojure "1.8.0"]
