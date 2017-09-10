8aeb891d70197dea473fcc648e2b6dabba8d6f3b
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-09-10 20:09:08 +0530

    Added dependencies.

--- project.clj
+++ project.clj
@@ -1,9 +1,22 @@
 (defproject rsvp-backend "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "RSVP Management System"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[bidi "1.19.0"]
+                 [clj-http "2.0.0"]
+                 [com.taoensso/timbre "2.7.1"]
+                 [com.taoensso/faraday "1.9.0"]
+                 [levand/immuconf "0.1.0"]
+                 [medley "0.7.0"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.3"]
+                 [org.clojure/tools.nrepl "0.2.6"]
+                 [ring-middleware-format "0.3.2" :exclusions [ring]]
+                 [ring/ring-core "1.5.0"]
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [ring/ring-json "0.4.0"]]
+  :plugins       [[refactor-nrepl "2.3.0-SNAPSHOT"]
+                 [cider/cider-nrepl "0.12.0"]]
   :main ^:skip-aot rsvp-backend.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
