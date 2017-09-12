fb3b1972dd61a4aeb8d70a0a341c238b6da2d57e
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-09-12 21:23:39 +0530

    Add handlers for all routes and rename namespace.

---
+++ src/rsvp_backend/handlers/invitee.clj
@@ -0,0 +1,34 @@
+(ns rsvp-backend.handlers.invitee
+  (:require [cheshire.core :as json]
+            [clojure.walk :as clj-walk]
+            [ring.util.request :as req]
+            [ring.util.response :as res]
+            [rsvp-backend.db.invitee :as db]
+            [taoensso.timbre :as log]))
+
+(defn new-invitee [request]
+  (let [query (:query (:params request))]
+    (log/debug (:body request))
+    (res/response {:status "SUCCESS"
+                   :data {:code (db/create-invitee (clj-walk/keywordize-keys (:body request)))}})))
+
+(defn get-invitee [request]
+  (let [code (:code (:route-params request))]
+    (res/response {:status "SUCCESS"
+                   :data (db/get-invitee (Integer/parseInt code))})))
+
+(defn update-metadata [request]
+  (let [code (:code (:route-params request))]
+    (db/update-invitee-metadata (Integer/parseInt code) (clj-walk/keywordize-keys (:body request)))
+    (res/response {:status "SUCCESS"})))
+
+(defn update-rsvp [request]
+  (let [code (:code (:route-params request))
+        response (:response (clj-walk/keywordize-keys (:body request)))]
+    (db/update-rsvp-status (Integer/parseInt code) response)
+    (res/response {:status "SUCCESS"})))
+
+(defn update-details [request]
+  (let [code (:code (:route-params request))]
+    (db/update-invitee-details (Integer/parseInt code) (clj-walk/keywordize-keys (:body request))))
+  (res/response {:rsvp_state "FORM_SUBMITTED"}))
--- src/rsvp_backend/handlers/new_invitee.clj
+++
@@ -1,7 +0,0 @@
-(ns rsvp-backend.handlers.new-invitee
-  (:require [ring.util.response :as res]
-            [rsvp-backend.db.invitee :as db]))
-
-(defn handler [{:keys [params]}]
-  (let [query (:query params)]
-    (res/response [{:a "b"}])))
