b707ff5fc169c5f811308e970b11648ffc0ee887
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-09-12 21:26:01 +0530

    Add all routes and pasrse request body into JSON

--- src/rsvp_backend/app.clj
+++ src/rsvp_backend/app.clj
@@ -5,19 +5,24 @@
             [ring.adapter.jetty :as jetty]
             [ring.middleware.json :as json]
             [ring.middleware.params :as params]
-            [rsvp-backend.handlers.new-invitee :as new-invitee]
+            [rsvp-backend.handlers.invitee :as invitee]
             [rsvp-backend.util :as util]
             [rsvp-backend.middleware.cors :as cors]
             [rsvp-backend.middleware.logging :as logging]
             [taoensso.timbre :as log]))
 
 
-(def routes ["/" [["invitee/new" new-invitee/handler]
+(def routes ["/" [["invitee/new" invitee/new-invitee]
+                  [["invitee/" :code] invitee/get-invitee]
+                  [["invitee/" :code "/metadata"] invitee/update-metadata]
+                  [["invitee/" :code "/rsvp"] invitee/update-rsvp]
+                  [["invitee/" :code "/details"] invitee/update-details]
                   [true (fn [_] (util/error-response 404 "Page not found."))]]
              true (fn [_] (util/error-response 404 "Page not found."))])
 
 (def main-handler
   (-> (br/make-handler routes)
+      json/wrap-json-body
       json/wrap-json-response
       params/wrap-params
       logging/wrap-log-request-response
