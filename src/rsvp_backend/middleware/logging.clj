6e9ac60c218981946985bb3532d48ce063dd20ee
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-09-19 00:06:51 +0530

    Don't log request body.

--- src/rsvp_backend/middleware/logging.clj
+++ src/rsvp_backend/middleware/logging.clj
@@ -6,7 +6,7 @@
 
 (def standard-ring-request-keys
   [:server-port :server-name :remote-addr :uri :query-string :scheme
-   :headers :request-method :body :params :form-params :query-params])
+   :headers :request-method :params :form-params :query-params])
 
 (defn wrap-log-request-response
  "Logs requests to the server and responses with INFO level."
