1e76226a589506ecb9498460dc6ff77ae4330420
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-09-23 22:57:11 +0530

    Added latest jar and build artifacts.

---
+++ micro_rsvp/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker build -t micro_rsvp
+sudo docker run -d --restart always --log-opt max-size=100m -p 80:80 micro_rsvp
+sudo docker ps
