5e6946edb9411b47d50ad5ab38784312d429471e
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-09-23 22:09:55 +0530

    Add Dockerfile

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ rm -rf micro_rsvp
 mkdir micro_rsvp
 cp target/uberjar/rsvp-backend-0.1.0-SNAPSHOT-standalone.jar micro_rsvp/micro_rsvp.jar
 cp -r resources micro_rsvp/
+cp Dockerfile micro_rsvp/
 tar -czf micro_rsvp.tar.gz micro_rsvp
